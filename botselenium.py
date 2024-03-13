40dcbffdfeeea468bd860bbd36dcfbdd89802796
JOstin-Orbe <63017264+Jostiin@users.noreply.github.com>
2024-03-12 21:10:08 -0500

    Add files via upload

--- botselenium.py
+++ botselenium.py
@@ -48,10 +48,8 @@ class WebScrapingSRI:
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        firefox_binary = '/usr/local/bin/firefox'  # Ruta al ejecutable de Firefox
-        self.browser = webdriver.Firefox(firefox_binary=firefox_binary)
-
-        #self.browser =  webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        #self.browser = webdriver.Chrome(options=chrome_options,executable_path = os.getcwd()+"/chromedriver.exe")
+        self.browser =  webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         #self.browser = webdriver.Chrome(executable_path="/home/jostinorbe/google-chrome-stable_current_amd64.deb")
         #self.browser = webdriver.ChromeService(executable_path="/home/jostinorbe/google-chrome-stable_current_amd64.deb")
         #except:
