1f5a3c5b03041859911bf7638ce5ace1c9adc733
JOstin-Orbe <63017264+Jostiin@users.noreply.github.com>
2024-03-12 22:36:38 -0500

    Add files via upload

--- botselenium.py
+++ botselenium.py
@@ -54,12 +54,12 @@ class WebScrapingSRI:
 
         #chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36')
 
-        options=webdriver.FirefoxOptions()
-        service = Service(executable_path='/usr/local/bin/geckodriver')
-        options.add_argument('--headless=new')
-        options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36')
-        self.browser=webdriver.Firefox(service=service, options=options)
-        #self.browser = webdriver.Firefox(options = chrome_options,executable_path='/usr/local/bin/geckodriver')
+        #options=webdriver.FirefoxOptions()
+        #service = Service(executable_path='/usr/local/bin/geckodriver')
+        #options.add_argument('--headless=new')
+        #options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36')
+        #self.browser=webdriver.Firefox(service=service, options=options)
+        self.browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
         #except:
 
         #    firefox_options = Options()
